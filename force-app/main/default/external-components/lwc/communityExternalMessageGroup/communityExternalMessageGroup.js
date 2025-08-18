d69d139c1c4a7abf1b2a03ad8bbc063fcb68a584
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-08-18 15:43:03 +0200

    show header after one hour

--- force-app/main/default/external-components/lwc/communityExternalMessageGroup/communityExternalMessageGroup.js
+++ force-app/main/default/external-components/lwc/communityExternalMessageGroup/communityExternalMessageGroup.js
@@ -2,4 +2,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class CommunityExternalMessageGroup extends LightningElement {
     @api messages;
+
+    get decoratedMessages() {
+        return this.messages.map((msgWrap, index) => {
+            return {
+                ...msgWrap,
+                bubbleStyle: msgWrap.showHeader && index > 0 ? 'margin-top: var(--a-spacing-5);' : ''
+            };
+        });
+    }
 }
