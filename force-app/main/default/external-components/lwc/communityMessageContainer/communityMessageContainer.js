3b4b9fac17fbaabbcb6d2207f0325ade1908e3b9
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-08-19 12:03:17 +0200

    fix

--- force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
+++ force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
@@ -3,21 +3,16 @@ import { LightningElement, api } from 'lwc';
 export default class CommunityMessageContainer extends LightningElement {
     @api messageGroups;
 
-    get decoratedGroups() {
-        return this.messageGroups.map((group) => {
-            const decoratedMessages = group.messages.map((msgWrap, index) => ({
-                ...msgWrap,
-                isExternal: msgWrap.message.CRM_External_Message__c,
-                bubbleStyle: msgWrap.showHeader && index > 0 ? 'margin-top: var(--a-spacing-5);' : ''
-            }));
-
-            const nonEventMessages = decoratedMessages.filter((m) => !m.isEvent);
-
-            return {
-                ...group,
-                messages: decoratedMessages,
-                nonEventMessages: nonEventMessages
-            };
+    get decoratedBlocks() {
+        return (this.messageGroups || []).map((block) => {
+            if (block.type === 'group') {
+                const decoratedMessages = (block.groupMessages || []).map((msgWrap, idx) => ({
+                    ...msgWrap,
+                    bubbleStyle: msgWrap.showHeader && idx > 0 ? 'margin-top: var(--a-spacing-5);' : ''
+                }));
+                return { ...block, groupMessages: decoratedMessages, isGroup: true, isEvent: false };
+            }
+            return { ...block, isGroup: false, isEvent: true };
         });
     }
 }
