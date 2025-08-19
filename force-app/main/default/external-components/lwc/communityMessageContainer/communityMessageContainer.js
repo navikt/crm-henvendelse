3b989e1f22970a80ae64d1525c7d595b1c8d9a82
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-08-19 08:55:31 +0200

    moved function to parent

--- force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
+++ force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
@@ -2,4 +2,14 @@ import { LightningElement, api } from 'lwc';
 
 export default class CommunityMessageContainer extends LightningElement {
     @api messageGroups;
+
+    get decoratedGroups() {
+        return this.messageGroups.map((group) => ({
+            ...group,
+            messages: group.messages.map((msgWrap, index) => ({
+                ...msgWrap,
+                bubbleStyle: msgWrap.showHeader && index > 0 ? 'margin-top: var(--a-spacing-5);' : ''
+            }))
+        }));
+    }
 }
