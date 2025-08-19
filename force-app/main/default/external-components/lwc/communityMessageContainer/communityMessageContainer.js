97e80339a8d4c912f8ea9d7e74a65e192f1c35b4
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-08-19 10:24:24 +0200

    show message as event if type is Event

--- force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
+++ force-app/main/default/external-components/lwc/communityMessageContainer/communityMessageContainer.js
@@ -4,12 +4,20 @@ export default class CommunityMessageContainer extends LightningElement {
     @api messageGroups;
 
     get decoratedGroups() {
-        return this.messageGroups.map((group) => ({
-            ...group,
-            messages: group.messages.map((msgWrap, index) => ({
+        return this.messageGroups.map((group) => {
+            const decoratedMessages = group.messages.map((msgWrap, index) => ({
                 ...msgWrap,
+                isExternal: msgWrap.message.CRM_External_Message__c,
                 bubbleStyle: msgWrap.showHeader && index > 0 ? 'margin-top: var(--a-spacing-5);' : ''
-            }))
-        }));
+            }));
+
+            const nonEventMessages = decoratedMessages.filter((m) => !m.isEvent);
+
+            return {
+                ...group,
+                messages: decoratedMessages,
+                nonEventMessages: nonEventMessages
+            };
+        });
     }
 }
