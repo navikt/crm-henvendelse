fbb6a31b7d9914789795046127b14231e6e8fa96
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-08-27 21:00:58 +0200

    updated moda for screen readers

--- force-app/main/default/external-components/lwc/communityModal/communityModal.js
+++ force-app/main/default/external-components/lwc/communityModal/communityModal.js
@@ -1,78 +1,100 @@
 import { LightningElement, api } from 'lwc';
 
 export default class CommunityModal extends LightningElement {
-    @api showModal = false;
-    @api overrideFirstFocus = false;
-    @api hideFooterLine = false;
+    @api showFooterLine = false;
     @api hidePadding = false;
     @api hidePaddingMobile = false;
     @api showCloseButton = false;
 
     bufferFocus = false;
+    _showModal = false;
+    _lastFocusedElement = null;
 
-    closeModal() {
-        this.dispatchEvent(
-            new CustomEvent('modalclosed', {
-                detail: false
-            })
-        );
+    @api
+    get showModal() {
+        return this._showModal;
+    }
+    set showModal(val) {
+        this._showModal = val;
+        if (val) {
+            Promise.resolve().then(() => {
+                this._lastFocusedElement = document.activeElement;
+                this.focusFirstElement();
+            });
+        } else {
+            if (this._lastFocusedElement) {
+                // eslint-disable-next-line @lwc/lwc/no-async-operation, @locker/locker/distorted-window-set-timeout
+                setTimeout(() => {
+                    this._lastFocusedElement.focus();
+                }, 0);
+            }
+        }
     }
 
     renderedCallback() {
         if (this.bufferFocus) {
-            this.focusModal();
+            this.focusFirstElement();
         }
     }
 
-    dispatchFocusFirst() {
-        if (this.overrideFirstFocus === false) {
-            this.focusLoop();
+    focusFirstElement() {
+        const focusable = this.modalFocusableElements;
+        if (focusable.length) {
+            focusable[0].focus();
         } else {
-            const focusFirstEvent = new CustomEvent('focusfirst', {
-                detail: null
-            });
-            this.dispatchEvent(focusFirstEvent);
+            this.template.querySelector('.modal').focus();
         }
+        this.bufferFocus = false;
     }
 
-    dispatchFocusLast() {
-        const focusLastEvent = new CustomEvent('focuslast', {
-            detail: null
-        });
-        this.dispatchEvent(focusLastEvent);
-    }
-
-    @api
-    focusModal() {
-        const modal = this.template.querySelector('.modal');
-        if (modal) {
-            this.bufferFocus = false;
-            modal.focus();
-        } else {
-            this.bufferFocus = true;
+    focusLastElement() {
+        const focusable = this.modalFocusableElements;
+        if (focusable.length) {
+            focusable[focusable.length - 1].focus();
         }
-    }
-
-    @api
-    focusLoop() {
-        const modalFocusElement = this.template.querySelector('.modalFocus');
-        modalFocusElement.focus();
     }
 
     handleFocus(event) {
         if (event.target.classList.contains('lastFocusable')) {
-            this.dispatchFocusFirst();
-        }
-        if (event.target.classList.contains('firstFocusable')) {
-            this.dispatchFocusLast();
+            this.focusFirstElement();
+        } else if (event.target.classList.contains('firstFocusable')) {
+            this.focusLastElement();
         }
     }
 
-    get modalClasses() {
+    handleKeyDown(event) {
+        if (event.key === 'Escape') {
+            this.closeModal();
+        }
+    }
+
+    closeModal() {
+        this.dispatchEvent(new CustomEvent('modalclosed', { detail: false }));
+        this._showModal = false;
+    }
+
+    get modalClass() {
         return (
             'modal modalMobile overrides' +
             (!this.hidePaddingMobile ? '' : ' noHorizontalPadding-mobile') +
             (!this.hidePadding ? ' modal-padding' : ' remove-modal-padding')
         );
+    }
+
+    get modalFocusableElements() {
+        const modal = this.template.querySelector('.modal');
+        if (!modal) return [];
+        const selectors = [
+            'a[href]:not([tabindex="-1"]):not([disabled])',
+            'area[href]:not([tabindex="-1"]):not([disabled])',
+            'input:not([type="hidden"]):not([disabled]):not([tabindex="-1"])',
+            'select:not([disabled]):not([tabindex="-1"])',
+            'textarea:not([disabled]):not([tabindex="-1"])',
+            'button:not([disabled]):not([tabindex="-1"])',
+            '[tabindex]:not([tabindex="-1"]):not([disabled])'
+        ];
+        return Array.prototype.slice.call(modal.querySelectorAll(selectors.join(','))).filter(function (el) {
+            return el.offsetParent !== null;
+        });
     }
 }
