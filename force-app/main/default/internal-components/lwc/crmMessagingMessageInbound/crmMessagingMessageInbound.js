d266b4b9878ae77c746ba1c6fb185295a2d8ec9e
eirikfladby <eirik.fladby@gmail.com>
2025-11-03 10:29:57 +0100

    set font color to black due to slds2

--- force-app/main/default/internal-components/lwc/crmMessagingMessageInbound/crmMessagingMessageInbound.js
+++ force-app/main/default/internal-components/lwc/crmMessagingMessageInbound/crmMessagingMessageInbound.js
@@ -1,5 +1,5 @@
 import { LightningElement, api } from 'lwc';
 
-export default class BearTile extends LightningElement {
+export default class CrmMessagingMessageInbound extends LightningElement {
     @api message;
 }
